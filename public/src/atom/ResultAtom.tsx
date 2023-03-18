57493bc39cff90b5350e3f9ff7f7720874df45d6
rizki aprita <vallerydelexy@gmail.com>
2023-03-19 00:34:48 +0700

    removed nextjs from this repo

--- public/src/atom/ResultAtom.tsx
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const resultState = atom({
-    key: 'resultState', // unique ID (with respect to other atoms/selectors)
-    default: [], // default value (aka initial value)
-  });
--- public/src/components/trackingTab.tsx
+++
@@ -1,7 +0,0 @@
-export default function TrackingTab() {
-  return (
-    <>
-      under development
-    </>
-  );
-}
